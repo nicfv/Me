b41e19c084d7d87154d3a340390c5e1b96cad6fd
nicfv <nfmventura@gmail.com>
2024-01-15 08:45:55 -0800

    Round to 2 decimal places

--- docs/blog/posts/2024-01-15-interest-calc.js
+++ docs/blog/posts/2024-01-15-interest-calc.js
@@ -5,12 +5,18 @@ function getNum(id = '') {
     return +document.getElementById(id).value ?? 0;
 }
 /**
+ * Round a number to 2 decimal places.
+ */
+function round(n = 0) {
+    return Math.round(n * (100)) / 100;
+}
+/**
  * Calculate and output the annual interest rate.
  */
 function calculate() {
     const F = getNum('F'), P = getNum('P'), Y = getNum('Y'), f = getNum('f'),
         i = 100 * f * ((F / P) ** (1 / (Y * f)) - 1);
-    document.getElementById('i').textContent = 'Annual Interest Rate = ' + i + '%';
+    document.getElementById('i').textContent = 'Annual Interest Rate = ' + round(i) + '%';
 }
 /**
  * Add event listeners.
