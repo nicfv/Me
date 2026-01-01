9fd4efe60021d6d24f615408390acea8f543aa27
Nic <github@nicfv.com>
2025-12-31 17:43:37 -0800

    copy excerpt filter from ai

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,4 +2,9 @@ export default function (eleventyConfig) {
     eleventyConfig.setInputDirectory('www');
     // eleventyConfig.addPassthroughCopy('path/to/media');
     eleventyConfig.addFilter('log', any => console.log(any));
+    eleventyConfig.addFilter("excerpt", (post) => {
+        const content = post.replace(/(<([^>]+)>)/gi, ""); // Strip HTML tags
+        const paragraphs = content.split(/\n/);
+        return paragraphs[0].split(" ").slice(0, 30).join(" ") + "..."; // First 30 words
+    });
 }
