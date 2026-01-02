30f6691034690fb28a3d53750d0eb4e676d331f5
Nic <github@nicfv.com>
2026-01-02 14:31:54 -0800

    create slugify function

--- www/media/script.js
+++ www/media/script.js
@@ -1,1 +1,11 @@
-document.querySelectorAll('h1,h2,h3,h4,h5,h6').forEach(element => element.setAttribute('id', element.textContent.toLowerCase().replaceAll(/\s+/g, '-')));
+function slugify(str = '') {
+    return str
+        .normalize()
+        .toLowerCase()
+        .trim()
+        .replaceAll(/[^a-z0-9\s-]/g, '')
+        .replaceAll(/\s+/g, '-')
+        .replaceAll(/-+/g, '-');
+}
+
+document.querySelectorAll('h1,h2,h3,h4,h5,h6').forEach(element => element.setAttribute('id', slugify(element.textContent)));
