459afd195c98396947905bfb8c1f987edbc2bdb4
Nic <github@nicfv.com>
2026-01-01 18:22:26 -0800

    fix bugs were it wouldnt capture the first paragraph content

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,7 +3,7 @@ export default function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('www/media');
     eleventyConfig.addFilter('log', any => console.log(any));
     eleventyConfig.addFilter('excerpt', post => {
-        const firstPara = post.match(/<p[^>]*>(.+)<\/p>/);
+        const firstPara = post.match(/<p[^>]*>(.+?)<\/p>/s);
         if (firstPara) {
             return firstPara[1].replaceAll(/<[^>]+>/g, ''); // Strip HTML tags
         } else {
