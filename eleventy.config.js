9978fa55d6c1c3d45b6de22b6983814068cd6a70
Nic <github@nicfv.com>
2026-01-01 18:28:53 -0800

    no need to log, I guess!

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,6 @@
 export default function (eleventyConfig) {
     eleventyConfig.setInputDirectory('www');
     eleventyConfig.addPassthroughCopy('www/media');
-    eleventyConfig.addFilter('log', any => console.log(any));
     eleventyConfig.addFilter('excerpt', post => {
         const firstPara = post.match(/<p[^>]*>(.+?)<\/p>/s);
         if (firstPara) {
