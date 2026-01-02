2a0081f9280b5d3e58291afcaefd1e10408cf399
Nic <github@nicfv.com>
2026-01-02 13:55:08 -0800

    add logic to find the title of the post

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,14 @@
 export default function (eleventyConfig) {
     eleventyConfig.setInputDirectory('www');
     eleventyConfig.addPassthroughCopy('www/media');
+    eleventyConfig.addFilter('title', post => {
+        const firstHeader = post.match(/<(h[1-6])[^>]*>(.+?)<\/\1>/s);
+        if (firstHeader) {
+            return firstHeader[2].replaceAll(/<[^>]+>/g, ''); // Strip HTML tags
+        } else {
+            return 'Untitled';
+        }
+    });
     eleventyConfig.addFilter('excerpt', post => {
         const firstPara = post.match(/<p[^>]*>(.+?)<\/p>/s);
         if (firstPara) {
