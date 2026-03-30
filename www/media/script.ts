60a78d02245b264b812b48c32b482836a94bfc31
Nic <github@nicfv.com>
2026-03-29 21:38:05 -0700

    this typescript code works

--- www/media/script.ts
+++ www/media/script.ts
@@ -1,3 +1,8 @@
+interface Window {
+    MathJax: any;
+    hljs: any;
+}
+
 // Configure MathJax to allow inline $math$
 window.MathJax = {
     tex: {
@@ -9,7 +14,7 @@ window.MathJax = {
 // Highlight code blocks
 window.hljs.highlightAll();
 
-function slugify(str = '') {
+function slugify(str: string): string {
     return str
         .normalize()
         .toLowerCase()
@@ -20,25 +25,23 @@ function slugify(str = '') {
 }
 
 // Add slugs to all header elements and generate the table of contents
-const toc = document.getElementById('toc');
+const toc: HTMLElement = document.getElementById('toc') as HTMLElement;
 document.querySelectorAll('h1,h2,h3,h4,h5,h6').forEach(element => {
-    const slug = slugify(element.textContent);
-    const depth = +(element.tagName.match(/\d/)[0]);
+    const slug: string = slugify(element.textContent);
+    const depth: number = +(element.tagName.match(/\d/)![0]);
     element.setAttribute('id', slug);
-    if (toc) {
-        const tocLink = document.createElement('a');
-        toc.appendChild(tocLink);
-        tocLink.textContent = element.textContent;
-        tocLink.style.marginLeft = `${depth}rem`;
-        tocLink.setAttribute('href', `#${slug}`);
-    }
+    const tocLink: HTMLAnchorElement = document.createElement('a');
+    toc.appendChild(tocLink);
+    tocLink.textContent = element.textContent;
+    tocLink.style.marginLeft = `${depth}rem`;
+    tocLink.setAttribute('href', `#${slug}`);
 });
 
 // Post-process all hyperlinks to other markdown files
 document.querySelectorAll('a[href]').forEach(a => {
-    const currentHref = a.getAttribute('href');
-    const regex = /^\.\/(.+)\.md(#[a-z0-9-]+)?$/;
-    const match = currentHref.match(regex);
+    const currentHref: string = a.getAttribute('href') ?? '';
+    const regex: RegExp = /^\.\/(.+)\.md(#[a-z0-9-]+)?$/;
+    const match: RegExpMatchArray | null = currentHref.match(regex);
     if (match) {
         a.setAttribute('href', `/${slugify(match[1])}/${match[2] ?? ''}`);
     }
